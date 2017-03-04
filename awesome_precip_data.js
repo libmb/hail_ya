3fa4e8e16c47b785a1f43a020a721e42830554ee
Michelle Bergquist <caker25@gmail.com>
2017-03-04 15:45:12 -0700

    Test

--- awesome_precip_data.js
+++ awesome_precip_data.js
@@ -3,10 +3,29 @@
 console.log("in awesome data");
 const dataAsJSON = `{
   "Boston":
-  {"Saturday": "rain", "Tuesday": "rain", "Wednesday": "rain", "Thursday": "rain"}
+  {"Saturday": "sunny", "Sunday": "sunny", "Monday": "sunny", "Tuesday": "rain", "Wednesday": "rain", "Thursday": "rain", "Friday": "sunny"}
 },
   "Phoenix":
-
+  {"Sunday": "sunny", "Monday": "sunny", "Tuesday": "sunny", "Wednesday": "sunny", "Thursday": "sunny", "Friday": "sunny", "Saturday": "sunny"}
+},
+  "Oslo":
+  {"Sunday": "hail", "Monday": "hail", "Tuesday": "hail", "Wednesday": "hail", "Thursday": "hail", "Friday": "hail", "Saturday": "hail"}
+},
+  "Seattle":
+  {"Sunday": "rain", "Monday": "rain", "Tuesday": "rain", "Wednesday": "rain", "Thursday": "rain", "Friday": "rain", "Saturday": "rain"}
+},
+  "NewYork":
+{"Sunday": "sunny", "Monday": "sunny", "Tuesday": "rain", "Wednesday": "rain", "Thursday": "rain", "Friday": "rain", "Saturday": "rain"}
+},
+  "Portland":
+  {"Sunday": "rain", "Monday": "rain", "Tuesday": "rain", "Wednesday": "rain", "Thursday": "rain", "Friday": "rain", "Saturday": "rain"}
+},
+  "Boulder":
+  {"Sunday": "sunny", "Monday": "snow", "Tuesday": "snow", "Wednesday": "sunny", "Thursday": "rain", "Friday": "rain", "Saturday": "snow"}
+},
+  "Portland":
+  {"Sunday": "snow", "Monday": "snow", "Tuesday": "sunny", "Wednesday": "snow", "Thursday": "snow", "Friday": "snow", "Saturday": "snow"}
+}
 `
 
 const data = JSON.parse(dataAsJSON);
