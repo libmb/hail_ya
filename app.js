fc52c16592c8b9e2538c5fbbedb96d53d30d09a5
Michelle Bergquist <caker25@gmail.com>
2017-03-04 15:55:55 -0700

    Test

--- app.js
+++ app.js
@@ -3,13 +3,19 @@ $(document).ready(function() {
  });
 
 findLocation();
-console.log("app.js is loaded")
+console.log("app.js is loaded ", dataAsJSON)
 function findLocation(){
+<<<<<<< HEAD
+  $('#btn').click(function() {
+    // console.log(data.Boston.Tuesday);
+    var city_name = $('#cities').innerText()
+    console.log("WTF")
+=======
   // $('#btn').click(function() {
-    console.log(data.Boston.Tuesday);
+    console.log(dataAsJSON.Boston.Tuesday);
+>>>>>>> 1dbed04fff5ee439c81e80daf7f0be13d3368ad5
 
     var type;
-  // }) //close .click
 
 // $("#search").click(function(){
 //   event.preventDefault();
@@ -19,7 +25,8 @@ function findLocation(){
     // var city2 = split_city[1];
     // console.log(selected_city);
 
- //closes .click function
+ // })// closes .click function
+ })
 } //closes findLocation
 
 function forecast(weather) {
--- awesome_precip_data.js
+++ awesome_precip_data.js
@@ -1,6 +1,4 @@
-
-
-console.log("in awesome data");
+<<<<<<< HEAD
 const dataAsJSON = `{
   "Boston":
   {"Saturday": "sunny", "Sunday": "sunny", "Monday": "sunny", "Tuesday": "rain", "Wednesday": "rain", "Thursday": "rain", "Friday": "sunny"}
@@ -29,4 +27,22 @@ const dataAsJSON = `{
 `
 
 const data = JSON.parse(dataAsJSON);
-console.log("This is JSON data ",data);
+console.log("This is JSON data ", data);
+=======
+
+
+console.log("in awesome data");
+const dataAsJSON = {
+  "Boston": {"Saturday": "sunny", "Sunday": "sunny", "Monday": "sunny", "Tuesday": "rain", "Wednesday": "rain", "Thursday": "rain", "Friday": "sunny"},
+  "Phoenix": {"Sunday": "sunny", "Monday": "sunny", "Tuesday": "sunny", "Wednesday": "sunny", "Thursday": "sunny", "Friday": "sunny", "Saturday": "sunny"},
+  "Oslo": {"Sunday": "hail", "Monday": "hail", "Tuesday": "hail", "Wednesday": "hail", "Thursday": "hail", "Friday": "hail", "Saturday": "hail"},
+  "Seattle": {"Sunday": "rain", "Monday": "rain", "Tuesday": "rain", "Wednesday": "rain", "Thursday": "rain", "Friday": "rain", "Saturday": "rain"},
+  "NewYork": {"Sunday": "sunny", "Monday": "sunny", "Tuesday": "rain", "Wednesday": "rain", "Thursday": "rain", "Friday": "rain", "Saturday": "rain"},
+  "Portland": {"Sunday": "rain", "Monday": "rain", "Tuesday": "rain", "Wednesday": "rain", "Thursday": "rain", "Friday": "rain", "Saturday": "rain"},
+  "Boulder": {"Sunday": "sunny", "Monday": "snow", "Tuesday": "snow", "Wednesday": "sunny", "Thursday": "rain", "Friday": "rain", "Saturday": "snow"},
+  "Calgary": {"Sunday": "snow", "Monday": "snow", "Tuesday": "sunny", "Wednesday": "snow", "Thursday": "snow", "Friday": "snow", "Saturday": "snow"}
+};
+
+// const data = JSON.parse(dataAsJSON);
+console.log("This is JSON data ",dataAsJSON);
+>>>>>>> 1dbed04fff5ee439c81e80daf7f0be13d3368ad5
