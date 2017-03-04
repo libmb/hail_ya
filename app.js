4e1eb1b56b99653297138f40a4fd9bb2c977d255
Merril Jeffs <merril@Merrils-MacBook-Pro.local>
2017-03-04 15:55:54 -0700

    trying to get submit to work

--- app.js
+++ app.js
@@ -1,19 +1,19 @@
 $(document).ready(function() {
    $('select').material_select();
- });
 
-findLocation();
-console.log("app.js is loaded ", dataAsJSON)
+   findLocation()
+
+});
+
 function findLocation(){
-<<<<<<< HEAD
-  $('#btn').click(function() {
-    // console.log(data.Boston.Tuesday);
-    var city_name = $('#cities').innerText()
-    console.log("WTF")
-=======
+
+  $('#btn').submit(function(event) {
+    event.preventDefault()
+    //
+    // console.log($('#cities').val())
+    // console.log(city_name)
   // $('#btn').click(function() {
     console.log(dataAsJSON.Boston.Tuesday);
->>>>>>> 1dbed04fff5ee439c81e80daf7f0be13d3368ad5
 
     var type;
 
@@ -26,8 +26,12 @@ function findLocation(){
     // console.log(selected_city);
 
  // })// closes .click function
- })
-} //closes findLocation
+ // })
+ //closes findLocation
+  })
+}
+
+
 
 function forecast(weather) {
 
