2a6e4e47ec120b70948a49812b7f661d31f0bd27
Richard Beese <richardbeese@Richards-MacBook-Air.local>
2017-03-06 20:04:00 -0700

    Dumped geocode query in lieu of fetch

--- README.md
+++ README.md
@@ -3,9 +3,11 @@
 
 * Richard Beese
 * Merril Jeffs
-* Molly
-* Michelle
-* Jim
+* Molly Brooks
+* Michelle Bergquist
+* Jim Duncan
 
 * DESCRIPTION
 * This is an app that allows users to get weather information based on their location and time of their party, then receive drink recipes that will use the actual weather as one of the ingredients.  It takes in two inputs and hits two APIs to give the user the ultimate mother nature party.  
+* TECHNOLOGIES
+* JavaScript, HTML5, CSS3, jQuery, AJAX, Illustrator
--- app.js
+++ app.js
@@ -1,42 +1,39 @@
 $(document).ready(function() {
   $('select').material_select();
   $('.collapsible').collapsible();
+  getLocation();
 
 });
 
 console.log("We are in js.js");
 
-getLocation();
 
-var lat = "39.7616";
-var long = "-104.9622";
+// var lat = "39.7616";
+// var long = "-104.9622";
 
 
 function getLocation() {
   console.log('We are in get Location');
-  $("#button").click(function(){
+  $("#button").click(function(event){
+    event.preventDefault();
     console.log("Here in maps")
       var city = $('#city').val();
       console.log('this is the city', city);
-      var geocoder =  new google.maps.Geocoder();
-      geocoder.geocode( { 'address': city}, function(results, status) {
-        console.log("These are the results of geocode", results);
-            if (status == google.maps.GeocoderStatus.OK) {
-              lat = results[0].geometry.location.lat();
-              long = results[0].geometry.location.lng();
-              console.log("longitude", long);
-              console.log("latitude", lat);
-              var key = '5567e4950b5b3b4ea9088c5ac3639c6b';
+      fetch("https://maps.googleapis.com/maps/api/geocode/json?address="+city+"&key=AIzaSyBeVvUWaX8RnJtKf4EERvDSJX0NFpiW6po")
+        .then(function(response) {
+          return response.json().then(function(json) {
+            var lat = json.results[0].geometry.location.lat;
+            var long = json.results[0].geometry.location.lng;
+            console.log(lat, long);
+            getPrecip(lat, long);
+          })
+        })  //closes .then function response
 
-              getPrecip(key, lat, long);
-            } else {
-              alert("Something got wrong " + status);
-            }  //this closes else
-          });  //this closes geocoder.geocode function
   });  //this closes .click
-}  //this closes getLocation
+} //this closes getLocation
 
-function getPrecip(key, lat, long) {
+
+function getPrecip(lat, long) {
   var key = '5567e4950b5b3b4ea9088c5ac3639c6b';
   var day = findDay();
   console.log("Day", day);
@@ -61,25 +58,19 @@ function findDay() {
 }
 
 
-
-
-
-
 function forecast(type) {
-
-
-
+  console.log("We are in forecast");
   if (type === 'snow') {
-    $('.drinkImg').append("<a href='img/snow.png'>")
+    $('#drinkImg').append("<a href='img/snow.png'>")
   }
   else if (type === 'rain') {
-    $('.drinkImg').append("<a href='img/rain.png'>")
+    $('#drinkImg').append("<a href='img/rain.png'>")
   }
   else if (type === 'sunny') {
-    $('.drinkImg').append("<a href='img/sun-01.png'>")
+    $('#drinkImg').append("<a href='img/sun-01.png'>")
   }
   else if (type === 'hail') {
-    $('.drinkImg').append("<a href='img/hail.png'>")
+    $('#drinkImg').append("<a href='img/hail.png'>")
   }
-  else "Guess you can't drink today"
+  else  return "Guess you can't drink today"
 }
